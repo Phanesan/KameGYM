7b2e78bf9908cbc1c09b4b2c64045220d773e937
RubenJunco <128674160+RubenJunco@users.noreply.github.com>
2023-06-12 13:04:16 -0700

    Modifique un poco el editarInstructor para que se viera mas como el figma

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,9 +11,12 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JPanel;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 
 import main.java.panel.ClientesPago;
+import main.java.panel.ConsultarClases;
 import main.java.panel.ConsultarCliente;
+import main.java.panel.ConsultarTarifa;
 import main.java.panel.LobbyInstructores;
 import main.java.panel.CrearClase;
 import main.java.panel.CrearCliente;
@@ -21,7 +24,10 @@ import main.java.panel.CrearTarifa;
 import main.java.panel.Creditos;
 import main.java.panel.EditarClase;
 import main.java.panel.EditarCliente;
+import main.java.panel.EditarInstructor;
+import main.java.panel.EditarTarifa;
 import main.java.panel.HistorialAsistencia;
+import main.java.panel.HistorialInstructores;
 import main.java.panel.HistorialPagos;
 import main.java.panel.LobbyClases;
 import main.java.panel.LobbyCliente;
@@ -80,6 +86,7 @@ public class Main {
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
 				changePanel(frame, new Lobby(main, Lobby.userCredential));
+				
 			}
 		});
 		menu3.addActionListener(new ActionListener() {
